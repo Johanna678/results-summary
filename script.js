dfec2d78c59f3078844bac53834aa69325c7370f
Manamela Phuti Johanna <johannaphutimanamela@gmail.com>
2026-06-06 16:17:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    const categoriesList = document.querySelector('.categories');
+    categoriesList.innerHTML = '';
+
+    data.forEach(item => {
+      const li = document.createElement('li');
+      li.className = item.category.toLowerCase();
+      li.innerHTML = `
+        <span>${item.category}</span>
+        <span>${item.score} / 100</span>
+      `;
+      categoriesList.appendChild(li);
+    });
+
+   
+    const total = data.reduce((sum, item) => sum + item.score, 0);
+    const average = Math.round(total / data.length);
+
+    document.querySelector('.number').textContent = average;
+  })
+  .catch(error => console.error('Error loading data:', error));
